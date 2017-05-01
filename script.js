bcbcc0156acc7a2c2eb8249b12e15fd4855f9944
Nabila Fahreen <nfahreen@gmail.com>
2017-05-01 00:15:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1) Hiding
+
+$(document).ready(function(){
+    $("body").click(function(){
+    // $("h1").click(function(){ 
+        $("h1").hide(500);
+ // 2) Showing
+		$("h1").show(500);
+        });
+ 
+    });
+
+// 3) Slide Up/Down
+
+$(document).ready(function(){
+	$('body').hover(function(){
+	// $('.img-swap').click(function(){
+		$('.img-swap').slideUp('slow');
+		$('.img-swap').slideDown('slow');
+		// alert('all done');
+
+		});
+});
+
+//4)Replace
+$(document).ready(function(){
+	$('body').click(function(){
+	// $('#main').click(function(){
+		$('#main').replaceWith('<h1>Hello World!</h1>');
+	});
+});
+//5)Animate
+$(document).ready(function(){
+     $("body").mouseenter(function(){
+    // $(".box").mouseenter(function(){
+        $(".box").animate({
+        	height: '500px',
+        	width:'500px'
+        });
+    });
+});
+// Toggle
+$(document).ready(function(){
+    $("body").click(function(){
+    // $("#change").click(function(){
+        $("#hide").toggle(function(){
+        	alert('alert once toggle is complete');
+
+        });
+    });
+});
